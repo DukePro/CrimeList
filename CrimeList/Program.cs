6a2b97d4dc84035d076c935f7efa021bbcc711bc
D Egor <Mail.egor.d@gmail.com>
2023-12-02 22:57:53 +0300

    +

--- CrimeList/Program.cs
+++ CrimeList/Program.cs
@@ -18,7 +18,11 @@
     {
         public Criminal()
         {
-
+            Name = GetName();
+            Nationality = GetNationality();
+            Height = GetHeight();
+            Weight = GetWeight();
+            isImprisoned = GetImprisonmentStatus();
         }
 
         public string Name { get; private set; }
@@ -26,7 +30,6 @@
         public int Height { get; private set; }
         public int Weight { get; private set; }
         public bool isImprisoned { get; private set; }
-
 
         private string GetName()
         {
@@ -61,31 +64,158 @@
         "Таня Лапша",
         "Алиса Пирожок",
         "Вика Щи",
-        "Яна Пельменина"
+        "Яна Афёра",
+        "Витя Застрелю",
+        "Паша Кабриолет",
+        "Коля Чмырь",
+        "Миша Мафиозник"
             };
 
-            string name = criminalNames[Random.Next(0, criminalNames.Length - 1)];
+            string name = criminalNames[Utils.GetRandomNumber(criminalNames.Length - 1)];
             return name;
+        }
+
+        private string GetNationality()
+        {
+            string[] nationalityes = new string[]
+            {
+        "Русский",
+        "Украинец",
+        "Белорус",
+        "Казах",
+        "Узбек",
+        "Таджик",
+        "Туркмен",
+        "Киргиз",
+        "Армянин",
+        "Азербайджанец",
+        "Молдаванин",
+        "Грузин",
+        "Киргиз",
+        "Туркмен",
+        "Татарин",
+        "Башкир",
+        "Чуваш",
+        "Абхаз",
+        "Осетин",
+        "Крымчанин",
+        "Карел",
+        "Эстонец",
+        "Латыш",
+        "Литовец",
+        "Удмурт",
+        "Марий",
+        "Чеченец",
+        "Ингуш",
+        "Дагестанец",
+        "Калмык",
+        "Чеченец",
+        "Коми",
+        "Тувинец",
+        "Хакас",
+        "Якут",
+        "Бурят",
+        "Чукча",
+        "Эвенк",
+        "Ненец",
+        "Карачаевец",
+        "Бурят",
+        "Гагауз",
+        "Лак",
+        "Кумык",
+        "Каргинец",
+        "Авар",
+        "Татарстанец",
+        "Брянчанин",
+        "Кубанец",
+        "Сибиряк",
+        "Дончанин"
+            };
+
+            string nationality = nationalityes[Utils.GetRandomNumber(nationalityes.Length - 1)];
+            return nationality;
         }
 
         private int GetWeight()
         {
-            int weight = 0;
+            int minWeight = 50;
+            int maxWeight = 150;
 
-            return weight;
+            return Utils.GetRandomNumber(minWeight, maxWeight);
         }
 
-        private int GetHight()
+        private int GetHeight()
         {
-            int height = 0;
+            int minHeight = 150;
+            int maxHeight = 220;
 
-            return height;
+            return Utils.GetRandomNumber(minHeight, maxHeight);
+        }
+
+        private bool GetImprisonmentStatus()
+        {
+            bool isImprisoned = false;
+
+            if (Utils.GetRandomNumber(1) == 1)
+            {
+                isImprisoned = true;
+            }
+
+            return isImprisoned;
+        }
+    }
+
+    class DataBase
+    {
+        private int ammountOfRecords = 20;
+        private List<Criminal> _criminals = new List<Criminal>();
+
+        public void ShowAllCriminals()
+        {
+            string imprisonmentStatus;
+
+            for (int i = 0; i < _criminals.Count; i++)
+            {
+                if (_criminals[i].isImprisoned == true)
+                {
+                    imprisonmentStatus = "В тюрьме";
+                }
+                else
+                {
+                    imprisonmentStatus = "На свободе";
+                }
+
+                Console.WriteLine($"{_criminals[i].Name}, {_criminals[i].Nationality}, Рост {_criminals[i].Height}, Вес {_criminals[i].Weight}, {imprisonmentStatus}");
+            }
+        }
+
+        private void CreateCriminals()
+        {
+            for (int i = 0; i < ammountOfRecords; i++)
+            {
+                _criminals.Add(new Criminal());
+            }
+        }
+    }
+
+    class Utils
+    {
+        private static Random s_random = new Random();
+
+        public static int GetRandomNumber(int minValue, int maxValue)
+        {
+            return s_random.Next(minValue, maxValue);
+        }
+
+        public static int GetRandomNumber(int maxValue)
+        {
+            return s_random.Next(maxValue);
+
         }
 
     }
-
-
 }
+    
 
 //У нас есть список всех преступников.
 //В преступнике есть поля: ФИО, заключен ли он под стражу, рост, вес, национальность.
